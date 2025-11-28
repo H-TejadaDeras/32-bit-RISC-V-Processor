2bcb8134bb9549a925295f8207264f97b7e2d1e9
Henry Neftaly Tejada Deras <htejada@olin.edu>
2025-11-27 21:19:43 -0500

    Fixed auipc issue with test; working on fixing issue with jalr

--- tester_memories/misc_instruct_test.s
+++ tester_memories/misc_instruct_test.s
@@ -6,10 +6,10 @@
 add x2, x0, x0 # Set stack pointer
 lw x5, 0(x2) # 01234567 -> x5
 lw x6, 4(x2) # 76543210 -> x6
-lw x28, 44(x2) # 00000010 -> x28
-auipc x8, 16 # Move 4 Instructions down
-addi x9, x9, 10 # 61A80 -> 61A90
-addi x9, x9, 1 # 61A90 -> 61A91
+lw x28, 40(x2) # 00000010 -> x28
+auipc x8, 16
+addi x9, x9, 10 # 00000000 -> 0000000A
+addi x9, x9, 1 # 0000000A -> 0000000B
 jal x10, 8 # Move 2 instructions down
 lui x9, 400000
 jalr x10, -16(x28) # Move 4 instructions up
